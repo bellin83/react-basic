1efa3786ea26f0c3e58c03eff1321e92337e86e0
Austin.MacbookPro <bellin83@gmail.com>
2021-03-20 08:26:12 +0900

    :art: Convert hooks RSP.jsx

--- RSP.jsx
+++ RSP.jsx
@@ -1,5 +1,5 @@
 const React = require('react');
-const { Component } = require('react');
+const {useState, useRef, useEffect} = require('react');
 
 // 클래스의 경우 -> constructor -> render -> ref -> componentDidMount
 // -> (setState/props 바뀔 때 -> souldComponentUpdate -> render -> componentDidUpdate)
@@ -21,97 +21,63 @@ const computerChoice = (imgCoord) => {
   return Object.entries(rspCoords).find(v => v[1] === imgCoord)[0];
 };
 
-class RSP extends Component {
-  state = {
-    result: '',
-    imgCoord: '0',
-    score: 0,
-  };
+const RSP = () => {
+  const [result, setResult] = useState('');
+  const [imgCoord, setImgCoord] = useState(rspCoords.바위);
+  const [score, setScore] = useState(0);
+  const interval = useRef();
 
-  interval;
+  useEffect(() => { // componentDidMount, componentDidUpdate 역할(1:1 대응은 아님
+    interval.current = setInterval(changeHand, 100);
+    return () => {
+      clearInterval(interval.current);
+    }
+  }, [imgCoord]);
 
-  // 컴포넌트가 첫 렌더링 후, 여기에 비동기 요청 많이
-  componentDidMount() {
-    this.interval = setInterval(this.changeHand, 100);
-  }
-
-  // 리렌더링 후
-  componentDidUpdate(prevProps, prevState, snapshot) {
-  }
-
-  // 컴포넌트가 제거되기 직전함, 여기서 비동기 요청 정리
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  changeHand = () => {
-    const { imgCoord } = this.state;
-
+  const changeHand = () => {
     if (imgCoord === rspCoords.바위) {
-      this.setState({
-        imgCoord: rspCoords.가위,
-      });
+      setImgCoord(rspCoords.가위);
     } else if (imgCoord === rspCoords.가위) {
-      this.setState({
-        imgCoord: rspCoords.보,
-      });
+      setImgCoord(rspCoords.보);
     } else if (imgCoord === rspCoords.보) {
-      this.setState({
-        imgCoord: rspCoords.바위,
-      });
+      setImgCoord(rspCoords.바위);
     }
   };
 
-  onClickBtn = (choice) => () => {
-    const { imgCoord } = this.state;
-
-    clearInterval(this.interval);
+  const onClickBtn = (choice) => () => {
+    clearInterval(interval.current);
     const myScore = scores[choice];
     const cpuScore = scores[computerChoice(imgCoord)];
     const diff = myScore - cpuScore;
 
     if (diff === 0) {
-      this.setState({
-        result: '비겼습니다!',
-      });
+      setResult('비겼습니다!');
     } else if ([-1, 2].includes(diff)) {
-      this.setState((prevState) => {
-        return {
-          result: '이겼습니다!',
-          score: prevState.score + 1,
-        }
-      });
+      setResult('이겼습니다!');
+      setScore(prevScore => prevScore + 1);
     } else {
-      this.setState((prevState) => {
-        return {
-          result: '졌습니다.!',
-          score: prevState.score - 1,
-        }
-      });
+      setResult('졌습니다.!');
+      setScore(prevScore => prevScore - 1);
     }
 
     setTimeout(() => {
-      this.interval = setInterval(this.changeHand, 100);
+      interval.current = setInterval(changeHand, 100);
     }, 2000);
   };
 
-  render() {
-    const { result, score, imgCoord } = this.state;
+  return (
+    <>
+      <div id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }}></div>
+      <div>
+        <button id='rock' className='btn' onClick={onClickBtn('바위')}>바위</button>
+        <button id='scissor' className='btn' onClick={onClickBtn('가위')}>가위</button>
+        <button id='paper' className='btn' onClick={onClickBtn('보')}>보</button>
+      </div>
+      <div>{result}</div>
+      <div>현재 {score}점</div>
+    </>
+  );
 
-    return (
-      <>
-        <div id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }}></div>
-        <div>
-          <button id='rock' className='btn' onClick={this.onClickBtn('바위')}>바위</button>
-          <button id='scissor' className='btn' onClick={this.onClickBtn('가위')}>가위</button>
-          <button id='paper' className='btn' onClick={this.onClickBtn('보')}>보</button>
-        </div>
-        <div>{result}</div>
-        <div>현재 {score}점</div>
-      </>
-    );
-
-  }
-}
+};
 
 module.exports = RSP;
