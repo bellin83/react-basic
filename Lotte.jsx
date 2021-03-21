602653b3dc7d7967c0e20fc9467f098f00d2df0d
bellin83 <bellin83@gmail.com>
2021-03-22 08:29:51 +0900

    :art: Add Lotte.jsx file

---
+++ Lotte.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+const { Component } = require('react');
+
+class Lotte extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+module.exports = Lotte;
