88a85b370fb2584f1d04cd119cf2419b2490f5d6
bellin83 <bellin83@gmail.com>
2021-03-26 09:15:08 +0900

    :art: 최적화 판단 log 추가

--- Td.jsx
+++ Td.jsx
@@ -1,14 +1,17 @@
 const React = require('react');
-const { useCallback } = require('react');
+const { useCallback, useEffect, useRef } = require('react');
 
 const CLICK_CELL = 'CLICK_CELL';
 
 const Td = ({ rowIndex, cellIndex, dispatch, cellData }) => {
+  const ref = useRef([]);
+  useEffect(() => {
+    console.log(rowIndex === ref.current[0], cellIndex === ref.current[1], dispatch === ref.current[2], cellData === ref.current[3]);
+    ref.current = [rowIndex, cellIndex, dispatch, cellData];
+  }, [rowIndex, cellIndex, dispatch, cellData]);
+
   const onClickTd = useCallback(() => {
-    console.log(rowIndex, cellIndex);
-
     if (cellData) return;
-
     dispatch({ type: CLICK_CELL, row: rowIndex, cell:cellIndex });
   }, [cellData]);
 
