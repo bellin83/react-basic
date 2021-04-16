d8effb23aceff976e346ab4830d0fb5a73b55a4f
austin783 <austin.hwang@tosslab.com>
2021-04-16 09:16:44 +0900

    :art: 4.0 Fetching Movies from API

--- nomadcoders_movie_app_2019/src/App.js
+++ nomadcoders_movie_app_2019/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   state = { isLoading: true };
 
+  getMovies = async () => {
+    const movies = axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json"
+    );
+  };
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 1000);
+    this.getMovies();
   }
 
   render() {
